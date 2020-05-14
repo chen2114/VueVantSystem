7aba04f607e5d61b20c029c3c56b8d5dec1d5920
13655079137 <731003808@qq.com>
2020-05-14 11:49:00 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './libraries/router'
+import store from './libraries/store'
+import './libraries/vant'
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button
+      type="default"
+      disabled
+    >
+      默认按钮
+    </van-button>
+    <van-button
+      loading
+      type="primary"
+      loading-type="spinner"
+      loading-text="加载中..."
+    >
+      主要按钮
+    </van-button>
+    <van-button
+      round
+      icon="star-o"
+      type="info"
+    >
+      信息按钮
+    </van-button>
+    <van-button type="warning">
+      警告按钮
+    </van-button>
+    <van-button type="danger">
+      危险按钮
+    </van-button>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+<style lang="less">
+.home {
+  width: 100%;
+  height: 100%;
+  background-color: #f2f4f7;
+}
+</style>
